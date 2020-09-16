6e36b96e89acc3b75f89547adb3825b73ee4aaa9
Aaron Amodt <aaron@amodttech.com>
2020-09-16 22:47:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,9 +16,9 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.detect{|win| board[win[0]] == board[win[1]]}
- 
- 
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
   board.all?{|positions| positions != " "}
   board.all?{|positions| positions == ("X" ||"O")}
 end
