1c3fa087a03f9b548bd74b02e1d0fc2130f8f820
Aaron Amodt <aaron@amodttech.com>
2020-09-16 20:38:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,15 +5,15 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  top_row_win = [0,1,2]
-middle_row_win = [3,4,5]
-bottom_row_win = [6,7,8]
-left_vertical_win = [0,3,6]
-middle_vertical_win = [1,4,7]
-right_vertical_win = [2,5,8]
-forward_diagonal_win = [1,4,8]
-backward_diagnonal_win = [2,4,6]
-  ]
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  [0,3,6]
+  [1,4,7]
+  [2,5,8]
+  [1,4,8]
+  [2,4,6]
+]
 
 def won?(board)
   
